b39f69feba6c720d6f414622bd71ec2025623123
Bareet Singh <singhbareet@gmail.com>
2022-05-29 17:47:48 +0530

    added N/A for blank values

--- src/main/java/com/covidtracker/service/CoronaDataService.java
+++ src/main/java/com/covidtracker/service/CoronaDataService.java
@@ -1,5 +1,10 @@
 package com.covidtracker.service;
 
+import com.covidtracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URI;
@@ -9,43 +14,37 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.covidtracker.models.LocationStats;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
 
 @Service
 public class CoronaDataService {
-    private static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     public List<LocationStats> fetchData() throws IOException, InterruptedException {
-        List<LocationStats> newStats=new ArrayList<>();
+        List<LocationStats> newStats = new ArrayList<>();
 
-        HttpClient client=HttpClient.newHttpClient();
-        HttpRequest request=HttpRequest.newBuilder()
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
 
-        HttpResponse<String> response=client.send(request,HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        StringReader csvBodyReader=new StringReader(response.body());
-        String temp=null;
+        StringReader csvBodyReader = new StringReader(response.body());
+        String temp = null;
 
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
             LocationStats locationStat = new LocationStats();
-            temp=record.get("Province/State");
-            if(temp.equals(""))
-                temp="---------";
+            temp = record.get("Province/State");
+            if (temp.equals(""))
+                temp = "N/A";
             locationStat.setState(temp);
             locationStat.setCountry(record.get("Country/Region"));
-            temp=String.valueOf(Integer.parseInt(record.get(record.size()-1))-Integer.parseInt(record.get(record.size()-2)));
-            if(temp.equals("0"))
-                temp="---------";
+            temp = String.valueOf(Integer.parseInt(record.get(record.size() - 1)) - Integer.parseInt(record.get(record.size() - 2)));
+            if (temp.equals("0"))
+                temp = "N/A";
             locationStat.setLatestCases(temp);
-            locationStat.setLatestTotalCases(record.get(record.size()-1));
+            locationStat.setLatestTotalCases(record.get(record.size() - 1));
             newStats.add(locationStat);
 
         }
