8cc8d818ae20ea87c52c0fee0abaac4a7c3c070b
unknown <kapilprajapati061@gmail.com>
2022-05-30 09:49:06 +0530

    Modes done

--- src/main/java/com/covidtracker/models/LocationStats.java
+++ src/main/java/com/covidtracker/models/LocationStats.java
@@ -1,17 +1,43 @@
 package com.covidtracker.models;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+// import lombok.ToString;
 
-@Data
-@NoArgsConstructor
-@ToString
+// @Data
+// @NoArgsConstructor
+// @ToString
 public class LocationStats {
     private String state;
     private String country;
     private String latestCases;
     private String latestTotalCases;
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getLatestCases() {
+        return latestCases;
+    }
+    public void setLatestCases(String latestCases) {
+        this.latestCases = latestCases;
+    }
+    public String getLatestTotalCases() {
+        return latestTotalCases;
+    }
+    public void setLatestTotalCases(String latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+    public LocationStats() {
+    }
 
 }
 
